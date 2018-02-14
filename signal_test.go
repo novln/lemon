dafd81b31c3e2aae420d84e8eb62b559a891a25a
Thomas LE ROUX <thomas@november-eleven.fr>
2018-02-14 23:11:52 +0100

    refactor: Update unit test

--- signal_test.go
+++ signal_test.go
@@ -3,98 +3,104 @@ package lemon
 import (
 	"context"
 	"os"
+	"sync/atomic"
 	"syscall"
 	"testing"
 	"time"
 )
 
-func TestAddSignal(t *testing.T) {
+func TestSignal(t *testing.T) {
+	tests := map[string]TestHandler{
+		"Option":   SignalAddOption,
+		"Shutdown": SignalShutdown,
+	}
 
-	e, err := New(context.Background(), AddSignal(syscall.SIGUSR1), AddSignal(syscall.SIGINT), AddSignal(syscall.SIGUSR2))
+	for name, handler := range tests {
+		t.Run(name, Setup(handler))
+	}
+}
+
+func SignalAddOption(runtime *TestRuntime) {
+
+	engine, err := New(runtime.Context(),
+		AddSignal(syscall.SIGUSR1),
+		AddSignal(syscall.SIGINT),
+		AddSignal(syscall.SIGUSR2),
+	)
 	if err != nil {
-		t.Fatalf("An error wasn't expected: %s", err)
+		runtime.Error("An error wasn't expected: %s", err)
+	}
+	if engine == nil {
+		runtime.Error("Engine must be defined")
 	}
 
 	hasUSR1 := false
 	hasUSR2 := false
 
-	for _, s := range e.signals {
-		if s == syscall.SIGUSR1 {
+	for i := range engine.signals {
+		if engine.signals[i] == syscall.SIGUSR1 {
 			hasUSR1 = true
 		}
-		if s == syscall.SIGUSR2 {
+		if engine.signals[i] == syscall.SIGUSR2 {
 			hasUSR2 = true
 		}
 	}
 
 	if !hasUSR1 {
-		t.Fatal("Engine should listen on SIGUSR1 signal")
+		runtime.Error("Engine should listen on SIGUSR1 signal")
 	}
 
 	if !hasUSR2 {
-		t.Fatal("Engine should listen on SIGUSR2 signal")
+		runtime.Error("Engine should listen on SIGUSR2 signal")
 	}
 
-	t.Log("Engine's configuration has a correct signal listener.")
+	runtime.Log("Engine's configuration has a correct signal listener.")
 
 }
 
-func TestShutdownWithMultipleSignal(t *testing.T) {
+func SignalShutdown(runtime *TestRuntime) {
 
-	i := 0
+	counter := int64(0)
 
-	e, err := New(context.Background(), BeforeShutdown(func() {
-		t.Log("Engine has executed BeforeShutdown hook.")
-		i++
+	engine, err := New(context.Background(), BeforeShutdown(func() {
+		runtime.Log("Engine has executed BeforeShutdown hook.")
+		atomic.AddInt64(&counter, 1)
 	}))
-
 	if err != nil {
-		t.Fatalf("An error wasn't expected: %s", err)
+		runtime.Error("An error wasn't expected: %s", err)
+	}
+	if engine == nil {
+		runtime.Error("Engine must be defined")
 	}
 
-	h := &testHook{}
-	h.kill = make(chan struct{}, 1)
+	hook := &testHook{}
+	hook.kill = make(chan struct{}, 1)
 
-	e.interrupt = make(chan os.Signal, 1)
+	engine.Register(hook)
 
-	e.Register(h)
-
-	d := make(chan struct{}, 1)
-
+	engine.interrupt = make(chan os.Signal, 1)
 	go func() {
 		time.Sleep(200 * time.Millisecond)
-		e.interrupt <- syscall.SIGINT
+		engine.interrupt <- syscall.SIGINT
 		time.Sleep(20 * time.Millisecond)
-		e.interrupt <- syscall.SIGINT
+		engine.interrupt <- syscall.SIGINT
 	}()
 
-	go func() {
-
-		if err = e.Start(); err != nil {
-			t.Errorf("An error wasn't expected: %s", err)
-		}
-
-		defer func() {
-			d <- struct{}{}
-		}()
-
-		if i > 1 {
-			t.Fatal("Engine shouldn't shutdown twice.")
-		}
-
-		if i == 0 {
-			t.Fatal("Engine should shutdown.")
-		}
-
-		t.Log("Engine has shutdown once.")
-
-	}()
-
-	select {
-	case <-d:
-		t.Log("Engine has stopped.")
-	case <-time.After(600 * time.Millisecond):
-		t.Fatal("Engine should have stopped.")
+	err = engine.Start()
+	if err != nil {
+		runtime.Error("An error wasn't expected: %s", err)
 	}
+
+	shutdown := atomic.LoadInt64(&counter)
+
+	if shutdown > 1 {
+		runtime.Error("Engine shouldn't shutdown twice.")
+	}
+
+	if shutdown == 0 {
+		runtime.Error("Engine should shutdown.")
+	}
+
+	runtime.Log("Engine has shutdown once.")
 
 }
